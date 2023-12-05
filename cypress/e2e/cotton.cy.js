e7eaa761eb8a0d932ec6a61f45458b3aaf399d46
amallipudi <amallipudi@targray.com>
2023-12-05 15:06:33 +0530

    ccc

--- cypress/e2e/cotton.cy.js
+++ cypress/e2e/cotton.cy.js
@@ -2,9 +2,12 @@ describe('Azure Active Directory Authentication', () => {
     it('verifies the user logged in has the correct name', () => {
        cy.loginToAAD(Cypress.env('aad_username'), Cypress.env('aad_password'))
       
-       cy.visit('https://targray-corp-uat.crm3.dynamics.com/').wait(10000)
+       cy.visit('https://targray-corp-uat.crm3.dynamics.com/main.aspx?appid=5a338062-8977-ed11-81ac-0022486df36c&pagetype=entitylist&etn=tar_strip&viewid=65e28876-255c-ed11-9562-000d3af4fee0&viewType=1039').wait(10000)
        cy.viewport(1800, 1400)
-     cy.wait(38000) 
+       cy.get('.ag-row-first > .ag-cell-range-left > .ms-Stack > .ms-Checkbox').dblclick()
+      
+      
+     
      Cypress.on('uncaught:exception', (err, runnable) => {
         return false
     })
